31f5704789f16d7a814bd47df8731c4937aaa8d0
Matthew Moore <matt@thrivesmart.com>
2010-06-23 19:25:51 -0700

    display calculated values correctly in admin section for search metrics

--- public/js/admin/organic-search-metrics.js
+++ public/js/admin/organic-search-metrics.js
@@ -35,25 +35,25 @@ var OrganicSearchMetrics = {
           $('#total-users').text(data.total_users);
           var histogramHtmls = [];
           for (var key in data.results) {
-              histogramHtmls << '<div><span>'+key+'</span><span>: '+data.results[key]+'</span></div>'
+              histogramHtmls.push('<li><span>Users with '+key+'</span><span>: '+data.results[key]+' ('+Math.round(100*data.results[key]/data.total_users)+'%)</span></li>');
           }
           histogramHtmls.sort();
-          $('#histogram-text').html(histogramHtmls.join(''));
+          $('#histogram-text').html('<ul>'+histogramHtmls.join('').replace('_organic_searches', ' Organic Searches')+'</ul>');
       });
   },
   fetchSummaryLoop: function(cursor, currentData, callback) {
     var cursorParam = cursor == null ? {} : {'cursor': cursor};
     $.post(this.sourceUrl, cursorParam, function(data) {
 			if (data['count'] == 0) { $('#loading-msg').fadeOut(); callback(currentData); }
-	        else {
-	            mergedData = {}
-	            mergedData.total_users = currentData.total_users + data.total_users;
-	            mergedData.results = currentData.results;
-	            for (var key in data.results) {
-	                if (mergedData.results[key] == null) { mergedData.results[key] = 0; }
-	                mergedData.results[key] += data.results[key];
-	            }
-                for (var key in currentData) {  mergedData[key] = currentData[key] + data[key]; }
+      else {
+          mergedData = {};
+          mergedData.total_users = currentData.total_users + data.total_users;
+          mergedData.results = currentData.results;
+          for (var key in data.results) {
+              if (mergedData.results[key] == null) { mergedData.results[key] = 0; }
+              mergedData.results[key] += data.results[key];
+          }
+                
 				OrganicSearchMetrics.fetchSummaryLoop(data['cursor'], mergedData, callback);
 			}
 	}, 'json');
